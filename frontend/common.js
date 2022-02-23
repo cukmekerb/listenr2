45fd31b79f17a360caaae09bc479e0e243e13a8b
cukmekerb <cukmekerb@gmail.com>
2022-02-22 18:33:46 -0800

    really cool subscribe button

--- frontend/common.js
+++ frontend/common.js
@@ -1,9 +1,9 @@
 let templates = {}; // this caches the templates so they don't need to be fetched
 
 /** automatically fills out handlebars template
- * @param {String} template_url - the url to the handlebars template to use
+ * @param {string} template_url - the url to the handlebars template to use
  * @param {Object} data - the data to fill.
- * @param {String} target_selector - the selector of the element to put the final HTML. if null then the html is returned
+ * @param {string} target_selector - the selector of the element to put the final HTML. if null then the html is returned
  * @param {Object} handlebar_options - extra handlebars config
  */
 async function fill_template(template_url, data, target_selector, handlebar_options) {
